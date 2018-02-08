e64d065af9a1fbabca848cd8e34511b77ac8315c
Laxman <laxman_20@hotmail.com>
2018-02-08 16:53:55 -0500

    Allow user to select their profile picture and save the data into the device. As well as read and display the image.

--- briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileFragment.java
+++ briar-android/src/main/java/org/briarproject/briar/android/profile/ProfileFragment.java
@@ -51,15 +51,20 @@ public class ProfileFragment extends BaseFragment implements
 	private EditText lastName;
 	private EditText nickname;
 	private EditText email;
+	private ImageView selectedImage;
 
 	// Variables to write profile information to file
 	Context context;
 	SharedPreferences sharedPref;
 	SharedPreferences.Editor editor;
+	private Bitmap currentImage;
 	private String firstNameInput;
 	private String lastNameInput;
 	private String nicknameInput;
 	private String emailInput;
+
+	// The request code for choosing profile picture
+	static final int PICK_PROFILE_PICTURE_REQUEST = 1;
 
 	public static ProfileFragment newInstance() {
 
@@ -79,11 +84,13 @@ public class ProfileFragment extends BaseFragment implements
 		// Set title of fragment to Profile
 		getActivity().setTitle(R.string.profile_button);
 
-		// Find the save information button
+		// Find the save information button and image
 		Button profileButton = (Button) profileView.findViewById(R.id.action_create_profile);
+		selectedImage = (ImageView) profileView.findViewById(R.id.profilePic);
 
 		// Set the listeners
 		profileButton.setOnClickListener(this);
+		selectedImage.setOnClickListener(this);
 
 		// Use SharedPreferences to store and retrieve profile information in Key-Value Sets
 		context = getActivity();
@@ -103,6 +110,33 @@ public class ProfileFragment extends BaseFragment implements
 		nickname   = (EditText)profileView.findViewById(R.id.profile_nickname);
 		email   = (EditText)profileView.findViewById(R.id.profile_email);
 
+		// Once the user selects an image we are going to store it into the app and display it.
+		String filename = "profile_picture_final";
+		FileInputStream inputStream = null;
+
+		try {
+			// Going to see if a image exists with our file name.
+			// If it does exist set the profile pic
+			inputStream = context.openFileInput(filename);
+			Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+			selectedImage.setImageBitmap(bitmap);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			Toast.makeText(getActivity(), "There was an error while retrieving your profile pic!",
+					Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Close stream
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		// If there already exists profile information set the data of the fragment with the users data.
 		if(firstNameInput != null && lastNameInput != null && nicknameInput != null && emailInput != null){
 			firstName.setText(firstNameInput);
@@ -117,6 +151,13 @@ public class ProfileFragment extends BaseFragment implements
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
+			// If the user click the profile picture
+			case R.id.profilePic:
+				// Open their gallery and let them pick a picture
+				Intent selectProfilePicture = new Intent(Intent.ACTION_PICK);
+				selectProfilePicture.setType("image/*");
+				startActivityForResult(selectProfilePicture, PICK_PROFILE_PICTURE_REQUEST);
+				break;
 			// If the user clicks the save button
 			case R.id.action_create_profile:
 				// Replace the key value pairs in our file with the updated profile data
@@ -135,6 +176,53 @@ public class ProfileFragment extends BaseFragment implements
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
+		// Once the user selects an image store the image to file and update the fragment
+		// Make sure correct request
+		if (requestCode == PICK_PROFILE_PICTURE_REQUEST) {
+			// No errors during intent
+			if (resultCode == RESULT_OK) {
+				// Get the URI that points to the image
+				Uri profileUri = data.getData();
+
+				// File where the image will be stored
+				String filename = "profile_picture_final";
+				FileOutputStream outputStream = null;
+
+				// If an image was selected
+				if (profileUri != null) {
+					try {
+						// Get the image and set profile fragment with it
+						currentImage = MediaStore.Images.Media.getBitmap(
+								this.getActivity().getContentResolver(), profileUri);
+						selectedImage.setImageBitmap(currentImage);
+
+						// Convert the image to a png so its lossless and takes less space and store in file
+						outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+						currentImage.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+						Toast.makeText(getActivity(), "There was an error while opening image location!",
+								Toast.LENGTH_LONG).show();
+					} catch (Exception e) {
+						e.printStackTrace();
+						Toast.makeText(getActivity(), "There was an error while saving your image!",
+								Toast.LENGTH_LONG).show();
+					} finally {
+						// Close stream
+						try {
+							if (outputStream != null) {
+								outputStream.close();
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+							Toast.makeText(getActivity(), "There was an error while closing a file!",
+									Toast.LENGTH_LONG).show();
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
