f39ae8bf2a3d5dda9da200780c7bd416fffdd97a
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2018-02-21 16:52:27 -0500

    briar-android

--- briar-android/src/main/java/org/briarproject/briar/android/MyFirebaseInstanceIdService.java
+++
@@ -1,4 +0,0 @@
-package org.briarproject.briar.android;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-}
--- briar-android/src/main/java/org/briarproject/briar/android/MyFirebaseMessagingService.java
+++
@@ -1,4 +0,0 @@
-package org.briarproject.briar.android;
-		import com.google.firebase.messaging.FirebaseMessagingService;
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-}
