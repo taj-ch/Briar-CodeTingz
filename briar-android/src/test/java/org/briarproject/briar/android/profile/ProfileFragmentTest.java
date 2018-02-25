9079c3879e31467c8edcbb9126b3d57443fcacf9
Laxman <laxman_20@hotmail.com>
2018-02-24 21:32:15 -0500

    Added tests that check if all the necessary components are viewable, clickable and editable in ProfileFragment and ProfileActivity #25

--- briar-android/src/test/java/org/briarproject/briar/android/profile/ProfileFragmentTest.java
+++ briar-android/src/test/java/org/briarproject/briar/android/profile/ProfileFragmentTest.java
@@ -3,6 +3,7 @@ package org.briarproject.briar.android.profile;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.briarproject.briar.R;
@@ -11,36 +12,34 @@ import org.briarproject.briar.android.login.SetupActivity;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
-
-/**
- * Created by Laxman on 2/9/2018.
- */
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 21, application = TestBriarApplication.class,
         packageName = "org.briarproject.briar")
 public class ProfileFragmentTest {
 
+    @InjectMocks
     private ProfileFragment profileFragment = new ProfileFragment();
 
-    private TextView nickname;
+    private EditText nickname;
     private EditText firstName;
     private EditText lastName;
-    private EditText email;
+    private TextView email;
     private EditText description;
-
+    private ImageView profileImage;
     private Button createProfileAccount;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+
         startFragment(profileFragment, SetupActivity.class);
         assertNotNull(profileFragment);
 
@@ -51,17 +50,38 @@ public class ProfileFragmentTest {
         email = v.findViewById(R.id.profile_email);
         description = v.findViewById(R.id.profile_description);
         createProfileAccount = v.findViewById(R.id.action_create_profile);
+        profileImage = v.findViewById(R.id.profilePic);
     }
 
     @Test
-    public void testCreateProfile() {
+    public void testCreateProfileUI() {
         firstName.setText("John");
         lastName.setText("Doe");
-        email.setText("JD@hotmail.com");
+        nickname.setText("JD");
         description.setText("I am a journalist.");
 
-        // Confirm that the create account button is clickable
-        assertEquals(createProfileAccount.isEnabled(), true);
-        createProfileAccount.performClick();
+        // Validate that the inputs set are correct
+        assertEquals(firstName.getText(), "John");
+        assertEquals(lastName.getText(), "Doe");
+        assertEquals(description.getText(), "I am a journalist.");
+        assertEquals(nickname.getText(), "JD");
+
+        // Make sure the profile image is clickable to update image
+        createProfileAccount.isClickable();
+
+        // Make sure the create profile button is clickable
+        profileImage.isClickable();
+    }
+
+    @Test
+    public void testProfileUIisVisible() {
+
+        // Check that all the inputs are visible
+        assertEquals(nickname.getVisibility(), View.VISIBLE);
+        assertEquals(firstName.getVisibility(), View.VISIBLE);
+        assertEquals(lastName.getVisibility(), View.VISIBLE);
+        assertEquals(email.getVisibility(), View.VISIBLE);
+        assertEquals(description.getVisibility(), View.VISIBLE);
+        assertEquals(profileImage.getVisibility(), View.VISIBLE);
     }
 }
