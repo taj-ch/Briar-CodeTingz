f9744169017d9be59e0ca299d38399b56fffb307
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-03-17 18:27:25 -0400

    Added layout and templates for viewing image full screen through chat #76

---
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/FullScreenImageActivity.java
@@ -0,0 +1,15 @@
+package org.briarproject.briar.android.contact;
+
+		import android.os.Bundle;
+		import android.support.v7.app.AppCompatActivity;
+
+		import org.briarproject.briar.R;
+
+public class FullScreenImageActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fullscreenimage);
+	}
+}
