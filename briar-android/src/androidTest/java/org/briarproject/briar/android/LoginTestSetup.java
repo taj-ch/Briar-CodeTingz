9ec2be9330e6979d65cfa16a05c8ab514b2bec38
Laxman <laxman_20@hotmail.com>
2018-03-18 03:56:59 -0400

    #78 Create a helper class to make check if the user is logged in during the tests. Also another helper to assert the title of a toolbar

---
+++ briar-android/src/androidTest/java/org/briarproject/briar/android/LoginTestSetup.java
@@ -0,0 +1,20 @@
+package org.briarproject.briar.android;
+
+/*Sign out closes the app and crashes the android tests. Therefore, we'll just login
+* if the user isn't already logged in.*/
+public class LoginTestSetup {
+
+	private static Boolean alreadyLoggedIn=false;
+
+	private LoginTestSetup(){}
+
+	public static Boolean isUserAlreadyLoggedIn(){
+		if(alreadyLoggedIn==false)
+		{
+			alreadyLoggedIn = true;
+			return false;
+		}
+
+		return alreadyLoggedIn;
+	}
+}
