49f79c75b1de2141644c32595ff7966a27012b35
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-03-17 21:01:46 -0400

    Polish UI layout for fullscreen image display, added downloadable link, added back button

--- briar-android/src/main/java/org/briarproject/briar/android/contact/FullScreenImageActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/FullScreenImageActivity.java
@@ -1,8 +1,11 @@
 package org.briarproject.briar.android.contact;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,16 +13,18 @@ import com.bumptech.glide.Glide;
 import org.briarproject.briar.R;
 public class FullScreenImageActivity extends AppCompatActivity {
 
+	String URL;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fullscreenimage);
 		ImageView fullScreenImageView = (ImageView) findViewById(R.id.imageFullView);
+		Button download = (Button)findViewById(R.id.image_download_button);
 
 		Intent callingActivityIntent= getIntent();
 		Bundle b = callingActivityIntent.getExtras();
 
-		String URL = "";
 
 		//Set for url
 		if(b!=null)
@@ -34,6 +39,16 @@ public class FullScreenImageActivity extends AppCompatActivity {
 					.load(URL)
 					.into(fullScreenImageView);
 		}
+
+		download.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Sender_Name = getIntent().getStringExtra("name");
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(URL));
+				startActivity(i);
+			}
+		});
 
 	}
 }
