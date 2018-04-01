86561dcfb29b107b713855e0ffc279ff0292b2c0
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-04-01 13:26:44 -0400

    Removed unused code and unnecessary commented code

--- briar-android/src/main/java/org/briarproject/briar/android/contact/WebviewFile.java
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/WebviewFile.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,10 +13,9 @@ import org.briarproject.briar.R;
 public class WebviewFile extends AppCompatActivity {
 
 	String URL = "";
-	String FILE_NAME="";
+	String FILE_NAME= "";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		final WebView webview;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_webview_file);
 		Button download = (Button)findViewById(R.id.file_download_button);
@@ -32,7 +30,6 @@ public class WebviewFile extends AppCompatActivity {
 		download.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//Sender_Name = getIntent().getStringExtra("name");
 				Intent i = new Intent(Intent.ACTION_VIEW);
 				i.setData(Uri.parse(URL));
 				startActivity(i);
